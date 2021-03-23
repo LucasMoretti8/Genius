a4c9d3486a869f6ea5d4711c68107640a642b21b
LucasMoretti <lucasmoretti8@gmail.com>
2021-03-23 13:07:56 -0300

    Corrections

--- genius.py
+++ genius.py
@@ -1,21 +1,26 @@
 import random, time, sys, os, winsound
+
 stream = sys.stdout
+level = int
+answer = ""
+round = 0
+turn = ""
+
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-lvl = int
-ans = ""
-l = str
-clear()
-
-frequency = 2500  # Set Frequency To 2500 Hertz
+frequency = 1500  # Set Frequency To 2500 Hertz
 duration = 100  # Set Duration To 1000 ms == 1 second
 def beep(duration=100):
     winsound.Beep(frequency, duration)
 
-lvl = int(input("Selecione o número de fases >> "))
-os.system('cls' if os.name == 'nt' else 'clear')
+
+
+clear()
+
+level = int(input("Selecione o número de fases >> "))
+clear()
 
 dif = int(input("Selecione a dificuldade [ 1 - FÁCIL ] // [ 2 - MÉDIO ] // [ 3 - DIFÍCIL ] // >> "))
 if dif == 1:
@@ -25,9 +30,6 @@ elif dif == 2:
 elif dif == 3:
     dif = .500
 
-
-round = 0
-turn = ""
 
 clear()
 print("Começando em 3...")
@@ -46,15 +48,15 @@ print("Começando em 3...2...1... JÁ!!")
 beep(500)
 time.sleep(1)
 clear()
+game = True
+
 
 resultList = [] # Imprimir resultado por round
-while round < lvl:
-    ans = ""
+while round < level:
+    answer = ""
     n = random.randint(0,9)
     n2 = str(n)
     resultList.append(n2) # Inclui o valor n2 dentro de uma lista
-    # print("[[ " + n2 + " ]]")
-    #print(*resultList) # Imprime o resultado
 
     for x in range(len(resultList)):
         print("[[ " + resultList[x] + " ]]")
@@ -63,30 +65,16 @@ while round < lvl:
         clear()
 
     clear()
-    ans = input(" >> >> >> ")
+    answer = input(" >> >> >> ")
     turn += n2
     #print ("Gabarito: ", turn)
     #print("Input: ", ans)
-    if ans == turn:
+    if answer == turn:
         round += 1
         clear()
     else:
-        print("GAME OVER!!")
-        print("Gabarito: ", turn)
-        print("Input: ", ans)
+        print("GAME OVER!!","Gabarito: {}".format(turn),"Input: {}".format(answer), sep='\n')
         quit()
-print("PARABÉNS!!!")
-beep(80)
-beep(80)
-beep(80)
-beep(80)
-beep(80)
-beep(80)
-beep(80)
-beep(80)
-beep(80)
-beep(80)
-beep(80)
-beep(80)
-print ("Gabarito: ", turn)
-print("Input: ", ans)
+print("PARABÉNS!!!","Gabarito: {}".format(turn),"Input: {}".format(answer), sep='\n')
+for i in range(10):
+  beep(80)
